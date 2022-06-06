96e7b5b57dcc0a8adee1e7eb3d17b259db301f01
MrazTevin <millatevin@gmail.com>
2022-06-06 14:55:36 +0000

    m: using function printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ * Description: print statement with printf
+ */
+int main(void)
+{
+	char *ptr = "with proper grammar, but the outcome is a piece of art, \n";
+
+	printf("%s", ptr);
+
+	return (0);
+}
