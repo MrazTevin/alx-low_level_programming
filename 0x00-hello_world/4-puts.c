98c14788520a134b53a3599f0c8f1f94b6c5ad42
MrazTevin <millatevin@gmail.com>
2022-06-06 14:30:36 +0000

    m: print with puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ * Return: Always 0 (Success)
+ * description: a program that prints with the puts function
+ */
+int main(void)
+{
+	char *s = "Programming is like building a multilingual puzzle";
+
+	puts(s);
+
+	return (0);
+}
