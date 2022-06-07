b8c23fcbfc86a595780cd60ae8a16def8d8fe987
MrazTevin <millatevin@gmail.com>
2022-06-07 01:50:42 +0000

    m : print alphabet letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0 (Success)
+ *
+ */
+int main(void)
+{
+
+	int alpha;
+
+	for (alpha = 'a'; alpha = 'z'; alpha++)
+	{
+		putchar(alpha);
+	{
+
+	putchar('\n');
+	return (0);
+}
