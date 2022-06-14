864bdc96a04bdbbbf0454bc8adb6abe4b8539d04
MrazTevin <millatevin@gmail.com>
2022-06-14 23:15:16 +0000

    m: make few changes

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,13 +3,14 @@
 /**
 * _strlen - return the length of the string
 * @s: input string
-* 
+* Return - 0
 */
 
 int _strlen(char *s)
 {
+	char s[];
 	int i;
-	
-	for(i=0; s != '\0'; ++i); 
-	return 0;
+
+	for (i = 0; s[i] != '\0'; ++i);
+	return (0);
 }
