38fca2b1a5af2beea5983cb442144a40d68fb9b1
MrazTevin <millatevin@gmail.com>
2022-06-16 13:39:30 -0700

    m: make changes to files

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,14 +7,22 @@
  * @src: source
  * Return: 0 (success)
  */
-
 char *_strcat(char *dest, char *src)
 {
+    int count = 0, count2 = 0;
 
-	dest = "Hello";
-	src = "World! \n";
+    while (*(dest + count) != '\0')
+    {
+        count++;
+    }
 
-
-
-	return (0);
-}
+    while (count2 >= 0)
+    {
+        *(dest + count) = *(src + count2);
+        if (*(src + count2) == '\0')
+            break;
+        count++;
+        count2++;
+    }
+    return dest;
+  }
