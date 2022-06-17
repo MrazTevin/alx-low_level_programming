cff8064fabbcf1580d53b3199edba9668946b46a
MrazTevin <millatevin@gmail.com>
2022-06-16 19:43:50 -0700

    m: rewrite file

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,27 +7,22 @@
 
 char *leet(char *s)
 {
-int counter = 0, i;
-int lower_case[] = {97, 101, 111, 116, 108};
-int upper_case[] = {65, 69, 79, 84, 76};
+int count = 0, i;
+int low_letters[] = {97, 101, 111, 116, 108};
+int upp_letters[] = {65, 69, 79, 84, 76};
 int numbers[] = {52, 51, 48, 55, 49};
 
-while (*(s + counter) != '\0')
+while (*(s + count) != '\0')
 {
 for (i = 0; i < 5; i++)
 {
-
-if (*(s + counter) == lower_case[i] || *(s + counter) == upper_case[i])
+if (*(s + count) == low_letters[i] || *(s + count) == upp_letters[i])
 {
-*(s + counter) = numbers[i];
-
-if (*(s + counter) == lower_case[i] || *(s + counter) == upper_case[i])
-{
-*(s + counter) = numbers[i];
+*(s + count) = numbers[i];
 break;
 }
 }
-counter++;
+count++;
 }
 
 return (s);
