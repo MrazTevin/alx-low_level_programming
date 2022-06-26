3534f2d9a9f5f5bb2451fb18c09de808b0ff0073
MrazTevin <millatevin@gmail.com>
2022-06-26 05:38:31 -0700

    m: function takes one parameter

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of a number
+ * @n: number being tested
+ * Return: Always (0)
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+
+}
