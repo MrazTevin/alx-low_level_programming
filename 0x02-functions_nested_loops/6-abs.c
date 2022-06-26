9756b080de1d4ad70fcaa4d32a55ef73077be396
MrazTevin <millatevin@gmail.com>
2022-06-26 05:20:57 -0700

    m: program computes absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _abs - compute the absolute value of an integer
+ * @int: data type
+ * Return: Always 0
+ */
+
+int _abs(int)
+{
+	int n;
+
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+
+	return (0);
+
+}
