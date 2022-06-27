9b5662460d60582c9643e81f5372874d697c52e6
MrazTevin <millatevin@gmail.com>
2022-06-27 01:33:48 -0700

    m: program to duplicate string pointer

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,3 +1,28 @@
 #include "main.h"
+#include <stdlib.h>
 
+/**
+ * _strdp - return pointer to a newly allocated space in memory
+ * @str: string
+ * Return: pointer of duplicated string
+ */
 
+char *_strdup(char *str)
+{
+	char *new_str;
+	char *p;
+
+	int len = 0;
+
+	while (str[len])
+		len++;
+	new_str = malloc(len + 1);
+	p = new_str
+	if (new_str == NULL)
+	       return (NULL);
+	while (*str)
+		*p++ = *new_str++;
+		*p = '\0'; /* byte is null */
+	return (new_str);	
+
+}
