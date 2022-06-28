edd07356fcff9992187926366b77b072592d89cb
MrazTevin <millatevin@gmail.com>
2022-06-28 08:21:01 -0700

    m: program to free 2 dimensional array

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - function that free a 2D grid
+ * @grid: multidimensional array to be freed
+ * @height: height of the grid
+ */
+
+void free_grid(int **grid, int height) 
+{
+	if (grid != NULL && height !=0)
+	{
+		for (; height >= 0; height--)
+			free(grid[height]);
+		free(grid);
+	
+	}
+
+
+}
