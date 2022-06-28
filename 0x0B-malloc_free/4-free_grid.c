bd290f41a44278992239f000d79a8b729fb40d90
MrazTevin <millatevin@gmail.com>
2022-06-28 08:40:16 -0700

    m: format to betty coding style

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -7,14 +7,14 @@
  * @height: height of the grid
  */
 
-void free_grid(int **grid, int height) 
+void free_grid(int **grid, int height)
 {
-	if (grid != NULL && height !=0)
+	if (grid != NULL && height != 0)
 	{
 		for (; height >= 0; height--)
 			free(grid[height]);
 		free(grid);
-	
+
 	}
 
 
