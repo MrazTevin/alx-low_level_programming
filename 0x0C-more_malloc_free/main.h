ee78689e0dc43071b83c646f98b5a114b7b45542
MrazTevin <millatevin@gmail.com>
2022-07-01 19:56:43 -0700

    m: add calloc function prototype

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,5 +2,6 @@
 #define MAIN_H
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int *array_range(int min, int max);
+void *_calloc(unsigned int nmemb, unsigned int size);
 
 #endif
