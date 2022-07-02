34b156aa1ea20bcc00d6d06b53fe06547b3f5fd2
MrazTevin <millatevin@gmail.com>
2022-07-01 19:47:21 -0700

    m: rewrite program

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,15 +1,14 @@
 #include "main.h"
-#include <stlib.h>
+#include <stdlib.h>
 #include <stdio.h>
-#include <type.h>
+#include <ctype.h>
 
 /**
- * _is_zero - determine if any number is zero
- * @argv: argument vector
+ * _is_zero - determines if any number is zero
+ * @argv: argument vector.
  *
- * Return: no return
+ * Return: no return.
  */
-
 void _is_zero(char *argv[])
 {
 	int i, isn1 = 1, isn2 = 1;
