db87f7f58d934aa533575dd62d24182b5d78151c
MrazTevin <millatevin@gmail.com>
2023-10-25 22:03:24 +0000

    m: implement the int _pow_recursion that prints the value of x raised to the power of y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _pow_recursion - Returns the value of x raise to the power of y
+* @x: The base number
+* @y: The exponent
+*
+* Return: Result of x^y, or -1 if y is lower than 0
+*/
+
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1); /* Return -1 to indicate an error if y is lower than 0 */
+}
+if (y == 0)
+{
+return (1); /* x^0 is always 1 */
+}
+return (x * _pow_recursion(x, y - 1)); /*  Recursive call to calculate x^y */
+}
