51fa66aec721f0f4dad6c8c370a8ed71f948ea56
MrazTevin <millatevin@gmail.com>
2023-11-01 02:31:23 +0000

    fix: error, ISO C90 forbids mixed declarations and code

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 
 /**
-* create_array - creates an array of chars and
-*                initializes it with a specific char
+* create_array - creates an array of chars and initializes it with a
+* specific char
 * @size: size of the array
 * @c: char to initialize the array with
 *
@@ -10,21 +10,25 @@
 */
 char *create_array(unsigned int size, char c)
 {
+char *array;
+unsigned int i;
+
 if (size == 0)
 {
 return (NULL);
 }
 
-char *array = malloc(size * sizeof(char));
+array = malloc(size * sizeof(char));
 if (array == NULL)
 {
 return (NULL); /* Memory allocation failed */
 }
 
-for (unsigned int i = 0; i < size; ++i)
+for (i = 0; i < size; ++i)
 {
 array[i] = c;
 }
 
 return (array);
 }
+
